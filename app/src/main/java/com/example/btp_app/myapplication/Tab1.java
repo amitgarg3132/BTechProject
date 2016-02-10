67c7670a0a3106aa7e7b3f2f98b63265d9ad947a
amitgarg3132 <amitgarg3132@gmail.com>
2016-02-10 15:36:57 +0530

    added expandable view

--- app/src/main/java/com/example/btp_app/myapplication/Tab1.java
+++ app/src/main/java/com/example/btp_app/myapplication/Tab1.java
@@ -11,20 +11,62 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ExpandableListView;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by Amit garg on 1/24/2016.
  */
 public class Tab1 extends Fragment {
-    MyAdapter myAdapter;
-    ListView listview;
-    ArrayAdapter<String> adapter;
-    String[] item_name = {"Tropicana(Select flavours)","Coca-Cola 6 can pack","Maggi Masala Noodles","Surf Excel Quick wash"};
-    String[] item_quantity={"1 litre","330ml x 6 cans","4 x 98 gm super saver pack","1 kg pack"};
+    ExpandableListAdapter listAdapter;
+    ExpandableListView expListView;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
+
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+
+        // Adding child data
+        listDataHeader.add("Beverages");
+        listDataHeader.add("Food");
+        listDataHeader.add("Coming Soon..");
+
+        // Adding child data
+        List<String> top250 = new ArrayList<String>();
+        top250.add("The Shawshank Redemption");
+        top250.add("The Godfather");
+        top250.add("The Godfather: Part II");
+        top250.add("Pulp Fiction");
+        top250.add("The Good, the Bad and the Ugly");
+        top250.add("The Dark Knight");
+        top250.add("12 Angry Men");
+
+        List<String> nowShowing = new ArrayList<String>();
+        nowShowing.add("The Conjuring");
+        nowShowing.add("Despicable Me 2");
+        nowShowing.add("Turbo");
+        nowShowing.add("Grown Ups 2");
+        nowShowing.add("Red 2");
+        nowShowing.add("The Wolverine");
+
+        List<String> comingSoon = new ArrayList<String>();
+        comingSoon.add("2 Guns");
+        comingSoon.add("The Smurfs 2");
+        comingSoon.add("The Spectacular Now");
+        comingSoon.add("The Canyons");
+        comingSoon.add("Europa Report");
+
+        listDataChild.put(listDataHeader.get(0), top250); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), nowShowing);
+        listDataChild.put(listDataHeader.get(2), comingSoon);
+    }
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,15 +75,15 @@ public class Tab1 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.tab_1, container, false);
-        RecyclerView recList = (RecyclerView) v.findViewById(R.id.recyclerView1);
-        recList.setHasFixedSize(true);
-        LinearLayoutManager llm = new LinearLayoutManager(getActivity());
-        llm.setOrientation(LinearLayoutManager.VERTICAL);
-        recList.setLayoutManager(llm);
+        expListView = (ExpandableListView) v.findViewById(R.id.lvExp1);
 
-        myAdapter = new MyAdapter(Arrays.asList(item_name),Arrays.asList(item_quantity));
+        // preparing list data
+        prepareListData();
 
-        recList.setAdapter(myAdapter);
+        listAdapter = new ExpandableListAdapter(getActivity(), listDataHeader, listDataChild);
+
+        // setting list adapter
+        expListView.setAdapter(listAdapter);
         return v;
     }
 
